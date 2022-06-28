1a9f326206c969419df2e173f848a581eb9c8c40
guraogr <guraogr@gmail.com>
2022-06-28 23:54:17 +0900

    feat: recoil-rootを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import './App.css';
 import { Test } from './Text';
 
 const App = () => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Test />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Test />
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 };
 
